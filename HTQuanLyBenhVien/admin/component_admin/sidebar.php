c8f31a5b5cccdfb47eb124ce9bc382fb9defacd8
nguyenloi2003 <141950838+thachrambo76@users.noreply.github.com>
2024-10-29 19:55:03 +0700

    Add files via upload

---
+++ HTQuanLyBenhVien/admin/component_admin/sidebar.php
@@ -0,0 +1,69 @@
+<div class="sidebar pe-4 pb-3">
+            <nav class="navbar bg-light navbar-light">
+                <a href="index.html" class="navbar-brand mx-4 mb-3">
+                    <h3 class="text-primary"><i class="fa fa fa-user me-2"></i>ADMIN</h3>
+                </a>
+                <div class="navbar-nav w-100">
+                    <a href="index.html" class="nav-item nav-link active"><i class="fa fa-tachometer-alt me-2"></i>Dashboard</a>
+                    <div class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa fa-laptop me-2"></i>Quản lí</a>
+                        <div class="dropdown-menu bg-transparent border-0">
+                            <a href="#1" class="dropdown-item">Đơn chờ</a>
+                            <a href="#1" class="dropdown-item">Tất cả đơn</a>
+                            <a href="#1" class="dropdown-item">Đã hủy</a>
+                        </div>
+                    </div>
+                    <div class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa fa-laptop me-2"></i>Bác sĩ</a>
+                        <div class="dropdown-menu bg-transparent border-0">
+                            <a href="index.php?quanli=tao-phieu-kham" class="dropdown-item">Tạo phiếu khám</a>
+                            <a href="index.php?quanli=xem-benh-nhan" class="dropdown-item">Xem bệnh nhân</a>
+                            <a href="#1" class="dropdown-item">Đã hủy</a>
+                        </div>
+                    </div>
+                    
+                    <div class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa fa-th me-2"></i>Bệnh nhân</a>
+                        <div class="dropdown-menu bg-transparent border-0">
+                            <a href="taophieukham.php" class="dropdown-item">Thêm mới</a>
+                            <a href="#1" class="dropdown-item">Tất cả</a>
+                            
+                        </div>
+                    </div>
+                    <div class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fas fa-box me-2"></i>Lịch hẹn khám</a>
+                        <div class="dropdown-menu bg-transparent border-0">
+                            <a href="#1" class="dropdown-item">Thêm mới</a>
+                            <a href="#1" class="dropdown-item">Tất cả</a>
+                            
+                        </div>
+                    </div>
+                    
+                    <div class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fas fa-box me-2"></i>Thu ngân</a>
+                        <div class="dropdown-menu bg-transparent border-0">
+                            <a href="#1" class="dropdown-item">Tất cả khóa học</a>
+                            <a href="#1" class="dropdown-item">Thêm mới</a>
+                            
+                        </div>
+                    </div>
+                    <div class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fas fa-box me-2"></i>Tiếp tân</a>
+                        <div class="dropdown-menu bg-transparent border-0">
+                            <a href="#1" class="dropdown-item">Tất cả khóa học</a>
+                            <a href="#1" class="dropdown-item">Thêm mới</a>
+                            
+                        </div>
+                    </div>
+                    <div class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fas fa-video me-2"></i>Nhà thuốc</a>
+                        <div class="dropdown-menu bg-transparent border-0">
+                            <a href="#1" class="dropdown-item">Tất cả videoc</a>
+                            <a href="#1" class="dropdown-item">Thêm mới</a>
+                            
+                        </div>
+                    </div>
+                    
+                </div>
+            </nav>
+        </div>
