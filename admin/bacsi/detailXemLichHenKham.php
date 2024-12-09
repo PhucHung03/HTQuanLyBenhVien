592d04da1d56d06cd8689333984e630858af3784
nguyenloi2003 <141950838+thachrambo76@users.noreply.github.com>
2024-12-09 16:13:38 +0700

    Add files via upload

---
+++ admin/bacsi/detailXemLichHenKham.php
@@ -0,0 +1,36 @@
+<div class="detailXemLichHenKham">
+    <div class="container">
+        <div class="bg-light mt-5">
+                <h3 class="d-flex justify-content-center pt-4">Thông tin lịch hẹn bệnh nhân</h3>
+                <form action="" method="post">
+
+                    <div class="d-flex justify-content-between ">
+                        <div class="d-flex">
+                            <div class="detailThongTin pb-5">
+                                <h4 class="p-3">Thông tin bệnh nhân</h4>
+                                <p><strong>Tên Bệnh Nhân:</strong> Nguyễn Văn A</p>
+                                <p><strong>Mã khám bệnh:</strong> 21395123</p>
+                                <p><strong>Số điện thoại:</strong> 0123457689</p>
+                                <p><strong>Địa chỉ:</strong> 16 Võ Văn Kiệt, Phường 10, TP.HCM</p>
+                                <p><strong>Mã BHYT:</strong> 123871985123</p>
+                            </div>
+                        </div>
+                        <div class="d-flex">
+                            <div class="detailKhoaKham pb-5">
+                                <h4 class="p-3">Khoa Khám bệnh</h4>
+                                <p><strong>Khoa Khám: </strong>Xương khớp</p>
+                                <p><strong>Bác sĩ khám:</strong>Phạm Tấn An</p>
+                                <p><strong>Ngày khám: </strong>22/10/2024</p>
+                                <p><strong>Giờ khám: </strong>9:00</p>
+                                <p><strong>Mô tả tình trạng sức khỏe:</strong> Đau lưng, đốt sống</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-end p-3">
+                            <button type="reset" class="btn btn-danger me-2" name="btnHuy">Quay Lại</button>
+                            <button type="submit" class="btn btn-success" name="btnHoanThanhKham">Hoàn Thành Khám</button>
+                    </div>
+                </form>
+            </div>  
+    </div>
+</div>
