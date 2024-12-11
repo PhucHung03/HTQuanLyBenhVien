2ad3c5d96f360eba035242b4d2650b788306924c
NguyenDuyKhangg <148305243+NguyenDuyKhangg@users.noreply.github.com>
2024-12-12 06:05:32 +0700

    Delete QLBV directory

--- QLBV/user/getDoctorsByDepartment.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $pdo = new PDO("mysql:host=localhost;dbname=qlbenhvien", "usertmdt", "passtmdt");
-    $maKhoa = $_GET['maKhoa'] ?? '';
-    $stmt = $pdo->prepare("SELECT maBacSi, tenBacSi FROM bacsi WHERE maKhoa = ?");
-    $stmt->execute([$maKhoa]);
-    echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
-?>
