6cb85180167e220c0526953c5395e173525ca4aa
DongTuanAnh03 <tuanseun3@gmail.com>
2024-12-11 14:06:29 +0700

    Add files via upload

---
+++ admin/bacsi/xemBenhNhanPhuTrach.php
@@ -0,0 +1,47 @@
+<div class="xemBenhNhanPhuTrach">
+    <section class="container-fluid pt-4 px-4">
+        <div class="bg-light text-center rounded p-4">
+            <div id="top__header">
+                <strong id="title_header">Danh sách bệnh nhân phụ trách</strong>
+                <form class="d-flex">
+                    <input id="form__search" type="search" placeholder="Tìm kiếm bệnh nhân..." aria-label="Search">
+                    <button id="btn__search" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+                </form>
+                <div id="table__danhsach">
+                    <div class="row header">
+                        <div class="col-4 col-sm-1 stt">STT</div>
+                        <div class="col-4 col-lg-2">Mã bệnh nhân</div>
+                        <div class="col-4 col-lg-2">Họ và tên</div>
+                        <div class="col-4 col-lg-2">Ngày sinh</div>
+                        <div class="col-4 col-lg-2">Liên hệ</div>
+                        <div class="col-4 col-lg-2">Thao tác</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-4 col-sm-1 stt">1</div>
+                        <div class="col-4 col-lg-2">000001</div>
+                        <div class="col-4 col-lg-2">Nguyễn Văn A</div>
+                        <div class="col-4 col-lg-2">01/01/1980</div>
+                        <div class="col-4 col-lg-2">0123456789</div>
+                        <div class="col-4 col-lg-2"><button><a href="index.php?quanli=xem-chi-tiet-benh-nhan-phu-trach">Xem chi tiết</a></button></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-4 col-sm-1">2</div>
+                        <div class="col-4 col-lg-2">000002</div>
+                        <div class="col-4 col-lg-2">Trần Thị B</div>
+                        <div class="col-4 col-lg-2">02/10/1986</div>
+                        <div class="col-4 col-lg-2">0123456789</div>
+                        <div class="col-4 col-lg-2"><button><a href="index.php?quanli=xem-chi-tiet-benh-nhan-phu-trach">Xem chi tiết</a></button></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-4 col-sm-1">3</div>
+                        <div class="col-4 col-lg-2">000003</div>
+                        <div class="col-4 col-lg-2">Võ Minh Hiếu</div>
+                        <div class="col-4 col-lg-2">30/11/2000</div>
+                        <div class="col-4 col-lg-2">0123456789</div>
+                        <div class="col-4 col-lg-2"><button><a href="index.php?quanli=xem-chi-tiet-benh-nhan-phu-trach">Xem chi tiết</a></button></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
