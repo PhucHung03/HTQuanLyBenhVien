bb22c2ff9e6aadbade09d8c2095aa155017febe6
nguyenloi2003 <141950838+thachrambo76@users.noreply.github.com>
2024-12-11 12:51:57 +0700

    Add files via upload

---
+++ public/thanhtuu.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang chủ</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- link -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+    integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+
+    <script src="js/jquery-3.7.1.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <style>
+        section {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+  
+
+}
+
+.contain {
+    display: flex;
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 20px;
+    margin-top: 160px;
+}
+
+.content {
+    flex: 3;
+    padding-right: 20px;
+}
+
+h1 {
+    color: #3b82f6;
+    font-size: 28px;
+    margin-bottom: 20px;
+}
+
+h2 {
+    font-size: 20px;
+    margin-bottom: 10px;
+    color: #3b82f6;
+}
+
+p {
+    line-height: 1.6;
+    margin-bottom: 20px;
+    font-weight: bold;
+}
+
+strong {
+    color: #3b82f6;
+}
+
+.sidebar {
+    flex: 1;
+    padding-left: 20px;
+    border-left: 1px solid #444;
+}
+
+.sidebar ul {
+    list-style: none;
+    padding: 0;
+}
+
+.sidebar li {
+    margin-bottom: 10px;
+}
+
+.sidebar a {
+    text-decoration: none;
+    color: #3b82f6;
+}
+
+.sidebar a:hover {
+    text-decoration: underline;
+}
+
+
+    </style>
+</head>
+<body>
+    <!-- header -->
+    <header class="header">
+        <div class="container-fluid">
+            <div class="row">
+                    <div class="header__top">
+                        <ul>
+                            <li><button href="tel:19002003" style="border-radius:20px;background-color: rgb(10, 104, 10); border: none;" >
+                                <i class="fa fa-phone" style="color: #ffffff;"></i>	<span style="color: #ffffff; padding: 10px;">1900 2003</span>
+                                </button>
+                            </li>
+                            <li ><button  rel="tel:0284567890" style="border-radius:20px; background-color: rgb(243, 11, 11); border: none;">
+                                <i class="fa fa-heart" style="color: #ffffff;"></i>	<span style="color: #ffffff;padding: 10px;">Cấp cứu (028) 456 7890</span>
+                                </button>
+                            </li>
+                            <li><button href="tel:19002003" style="border-radius:20px;background-color: rgb(21, 176, 21); border: none;" >
+                                <i class="fa fa-calendar" style="color: #ffffff;"></i>	<span style="color: #ffffff;padding: 10px;">Đặt lịch khám</span>
+                                </button>
+                            </li>
+                            <li>
+                                <a href="">Đăng nhập / </a>
+                                <a href="">Đăng kí</a>
+                            </li>
+                        </ul>
+                    </div>
+            </div>
+        </div>
+        <div class="container-fluid header__menu">
+            <div class="row">
+                <div class="col-lg-4 col-sm-3 " style="text-align: center; padding-bottom: 5px;">
+                    <div class="header__img">
+                        <img src="img/sang tao hospital.png" alt="" style="width: 300px;">
+                    </div> 
+                </div>
+                <div class="col-lg-8 col-sm-3 ml-5">
+                    <div class="header__right">
+                        <div class="header__menu">
+                            <ul>
+                                <li><a href=""><h5>TRANG CHỦ</h5></a></li>
+                                <li><a href=""><h5>CHUYÊN KHOA</h5></a></li>
+                                <li><a href="bacsi.php"><h5>BÁC SĨ</h5></a></li>
+                                <li><a href=""><h5>DỊCH VỤ</h5></a>
+                                    <ul class="dropdown">
+                                        <li><a href="">Bảng giá dịch vụ</a></li>
+                                        <li><a href="">Quy trình nhập viện</a></li>
+                                        <li><a href="">Quy trình xuất viện</a></li>
+                                        <li><a href="">Liên hệ với chúng tôi</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="thanhtuu.php"><h5>THÀNH TỰU</h5></a></li>
+                                <li><a href=""><h5>TIN TỨC</h5></a></li>
+                                <li><a href=""><h5>BỆNH NHÂN</h5></a>
+                                    <ul class="dropdown">
+                                        <li><a href="">Đặt lịch khám</a></li>
+                                        <li><a href="">Tra cứu thông tin</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+    
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <section>
+        <div class="contain">
+            <div class="content">
+                <h1>Thành tựu nổi bật của Bệnh viện Đa khoa Sáng Tạo</h1>
+                <p>Trên hành trình hơn 10 năm hình thành và phát triển, Bệnh viện Đa khoa Sáng Tạo không ngừng khẳng định vị thế hàng đầu trong lĩnh vực chăm sóc sức khỏe, ghi dấu bằng những thành tựu nổi bật.</p>
+                <p>Hoạt động với sứ mệnh <strong>“Phụng sự vì sức khỏe cộng đồng”</strong> cùng cam kết <strong>“Tận tâm dốc lòng vì sức khỏe Việt”</strong>, chúng tôi luôn lấy người bệnh làm trọng tâm, nỗ lực phát huy những giá trị cốt lõi bền vững.</p>
+                <p>Lần đầu tiên Bệnh viện Đa khoa Sáng Tạo ra mắt 3 Trung tâm Kỹ thuật cao – tạo bước ngoặt lớn chuyển mình trong lĩnh vực y tế.</p>
+                <p>Với sứ mệnh <strong>“Chữa bệnh – cứu người”</strong>, Bệnh viện Đa khoa Sáng Tạo trong suốt 10 năm qua đã dốc sức, tận tâm vì sức khỏe cộng đồng. Không dừng lại ở đó, Bệnh viện còn hướng tới những dịch vụ tốt nhất, mong muốn đem đến cho khách hàng, người bệnh những sản phẩm y tế chất lượng, đẳng cấp, hoàn hảo nhất.</p>
+                <h2>Bước ngoặt lớn với 3 Trung tâm Kỹ thuật cao</h2>
+                <p>
+                    Trong hành trình đổi mới, Bệnh viện Đa khoa Sáng Tạo tự hào ra mắt 3 Trung tâm Kỹ thuật cao, tạo nên bước chuyển mình đột phá trong ngành y tế. Những trung tâm này được đầu tư mạnh mẽ, đưa vào áp dụng các kỹ thuật hiện đại nhất, nâng tầm chất lượng dịch vụ y tế và mang lại hiệu quả điều trị vượt trội.
+                </p>
+                <h2>Trung tâm Hiếm muộn & Y học giới tính (IVF Sáng Tạo):</h2>
+                <p>
+                    Phòng Lab IVF hiện đại bậc nhất miền Bắc với tiêu chuẩn phòng sạch ISO 6 và hệ thống lọc khí tiên tiến, đảm bảo môi trường lý tưởng cho sự phát triển phôi. Hệ thống kính ICSI thế hệ mới, kết hợp với các thiết bị kiểm soát chất lượng không khí định kỳ, giúp gia tăng tỷ lệ đậu thai, mang lại hy vọng cho hàng nghìn cặp đôi vô sinh hiếm muộn.
+                </p>
+                <h2>Trung tâm Vú Kỹ thuật cao Mammocare:</h2>
+                <p>
+                    Tiên phong trong chẩn đoán và điều trị ung thư vú, trung tâm sở hữu công nghệ X-quang vú 3D Mammography và Siêu âm vú 3D Acuson S2000 hiện đại. Quy trình khép kín, chuyên nghiệp hóa mọi giai đoạn, từ tư vấn đến điều trị, giúp tiết kiệm thời gian và tối ưu hiệu quả chẩn đoán, điều trị.
+                </p>
+                <h2>Trung tâm Chẩn đoán và Can thiệp Tiêu hóa – Gan – Mật:</h2>
+                <p>
+                    Sử dụng công nghệ Narrow Band Imaging (NBI) thế hệ mới với độ chính xác cao trong chẩn đoán sớm ung thư đường tiêu hóa. Hệ thống nội soi 4K và công nghệ không đau đã mang lại trải nghiệm điều trị tốt nhất cho người bệnh, tạo tiền đề quan trọng cho những bước đột phá trong lĩnh vực tiêu hóa.
+                </p>
+            </div>
+            <aside class="sidebar">
+                <h2>Chuyên mục</h2>
+                <ul>
+                    <li><a href="#">Báo chí nói gì về chúng tôi</a></li>
+                    <li><a href="#">Các kỹ thuật điều trị</a></li>
+                    <li><a href="#">Chấn thương cột sống</a></li>
+                    <li><a href="#">Chương trình khuyến mại</a></li>
+                    <li><a href="#">Cơ xương khớp</a></li>
+                    <li><a href="#">Con Số và Những Câu Chuyện</a></li>
+                    <li><a href="#">Gen di truyền</a></li>
+                    <li><a href="#">Hỗ trợ sinh sản</a></li>
+                    <li><a href="#">Hỏi đáp chuyên gia</a></li>
+                </ul>
+            </aside>
+        </div>
+
+    </section>
+
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 footer__descrice">
+                    <h4>Bệnh viện Sáng Tạo</h4>
+                    <hr>
+                    <p>Số 3, đường 17A, P. Bình Trị Đông B, Quận Bình Tân, TP.HCM</p>
+                    <p>Tổng đài: 1900 8146</p>
+                    <p>Cấp cứu 24/7: (028) 6290 1155</p>
+                    <p>Email: sangtaoHospital@st.com.vn</p>    
+                </div>
+                <div class="col-lg-4 footer__system" >
+                    <h4>Dịch vụ</h4>
+                    <ul>
+                        <li><a href="#">Chuyên Khoa</a></li>
+                        <li><a href="#">Gói dịch vụ </a></li>
+                        <li><a href="#">Bảo hiểm</a></li>
+                        <li><a href="#">Đặt lịch hẹn</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 footer__search" >
+                  <div><img src="img/logoFooter.png" alt="logoFooter"></div>
+                  <div><p>THEO DÕI TIN TỨC VÀ DỊCH VỤ MỚI NHẤT CỦA CHÚNG TÔI</p></div>
+                  <div class=" email-container">
+                    <label><input class="text-content-footer input-email form-control" type="email" placeholder="Email của bạn"></label>
+                    <button class="btn btn-success">Gửi</button>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    <div class="footer__copyright__text">
+                        <p>Bản quyền &copy; 2024 thuộc về Bệnh viện Sáng Tạo <i class="fa fa-heart" aria-hidden="true"></i> by <a href="#" target="_blank">Sáng Tạo Hospital</a></p>
+                    </div>
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                </div>
+            </div>
+        </div>
+     </footer>
+
